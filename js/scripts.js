4687b29ab7c4949e321942c37ae94b657c948fad
David Lewis and Ian Goodrich <ian@>
2018-01-18 16:20:11 -0800

    Finish adding to JS function to account of punctuation and words beginning with qu

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-var vowels = ["a", "e", "i", "o", "u"];
-var outputArray= [];
-
 // function translate(phrase) {
 // for (index = 0; index < phrase.length; index++) {
 //   for (i = 0; i < vowels.length; i++ )
@@ -26,27 +23,49 @@ var outputArray= [];
 
 
 
-
+var vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
+var outputArray = [];
 function work(word) {
-  ourWord = (word.toLowerCase()).split(" ");
+  ourWord = word.split(" ");
   ourWord.forEach(function(word) {
-    letterArray = word.split("");
-    for (x = 0; x < vowels.length; x++) {
-      for (i = 0; i < letterArray.length; i++) {
+    letterArray = (word.toLowerCase().replace(/\W+/g, "")).split("");
+    letterArray[0]= letterArray[0].toUpperCase();
+    for (i = 0; i < letterArray.length; i++) {
+      for (x = 0; x < vowels.length; x++) {
         if (letterArray[i] === vowels[x]) {
           targetLetter = i;
-          x += 100;
-          if (targetLetter === 0) {
+          i += 100;
+          if (targetLetter !== x) {
+            var newWord = letterArray.join("");
+            var first = newWord.slice(0,1);
+            var last = newWord.slice(1);
+            var final = last.concat(first + "ay");
+
+          } else if (targetLetter === 0) {
             var newWord = letterArray.join("");
             var final = newWord.concat("way");
-            console.log(targetLetter, newWord, final)
+            console.log(targetLetter, newWord, final);
+
+          } else if (letterArray[0] === 'Q' && letterArray[1] === "u" || letterArray[1] === "U"){
+            var newWord = letterArray.join("");
+            var first = newWord.slice(0, 2);
+            var last = newWord.slice(2);
+            var final = last.concat(first + "ay");
+
           } else if (targetLetter !== 0) {
             var newWord = letterArray.join("");
             var first = newWord.slice(0, targetLetter);
             var last = newWord.slice(targetLetter);
             var final = last.concat(first + "ay");
-            console.log(targetLetter, newWord, final);
+            console.log(letterArray, newWord, final);
+
+          } else {
+            var newWord = letterArray.join("");
+            var first = newWord.slice(0, 1);
+            var last = newWord.slice(1);
+            var final = last.concat(first + "ay");
           };
+
           outputArray.push(final);
         }
       }
